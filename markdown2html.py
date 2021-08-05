46692160d97e1f59bf57df0fa0f869547a6f3a89
luismvargasg <1610@holbertonschool.com>
2021-08-05 17:11:40 -0500

    adding task 0

---
+++ markdown2html.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" Script that converts Markdown to HTML """
+
+
+from genericpath import isfile
+import sys
+import os
+
+
+def markdown2html():
+    """
+    Script that converts Markdown to HTML
+
+    Args:
+        First argument is the name of the Markdown file.
+        Second argument is the output file name.
+
+    Returns:
+        Nothing.
+    """
+    if (len(sys.argv) < 3):
+        print("Usage: ./markdown2html.py README.md README.html",
+              file=sys.stderr)
+        sys.exit(1)
+
+    md_file = sys.argv[1]
+    html_file = sys.argv[2]
+
+    if not(os.path.isfile("./" + md_file)):
+        print("Missing {}".format(md_file), file=sys.stderr)
+        sys.exit(1)
+
+    sys.exit(0)
+
+if __name__ == "__main__":
+    markdown2html()
