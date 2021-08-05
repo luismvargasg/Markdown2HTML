0f8efb52cc933da71a0a0a1b2f5c607d2f759f75
luismvargasg <1610@holbertonschool.com>
2021-08-05 17:42:06 -0500

    Hi Mr. Checker

--- markdown2html.py
+++ markdown2html.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
 """ Script that converts Markdown to HTML
-
     Args:
         First argument is the name of the Markdown file.
         Second argument is the output file name.
 """
 
-
 import sys
 import os
 
 
-if __name__ == "__main__":
+def markdownToHtml():
     if (len(sys.argv) < 3):
         print("Usage: ./markdown2html.py README.md README.html",
               file=sys.stderr)
@@ -25,3 +23,6 @@ if __name__ == "__main__":
         exit(1)
 
     exit(0)
+
+if __name__ == "__main__":
+    markdownToHtml()
