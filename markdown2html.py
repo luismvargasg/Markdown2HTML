2d14add293a7dfe5aea3c44d1ebbf37f3366bf5e
luismvargasg <1610@holbertonschool.com>
2021-08-05 17:31:02 -0500

    Testing the checker

--- markdown2html.py
+++ markdown2html.py
@@ -1,34 +1,27 @@
 #!/usr/bin/python3
-""" Script that converts Markdown to HTML """
+""" Script that converts Markdown to HTML
+
+    Args:
+        First argument is the name of the Markdown file.
+        Second argument is the output file name.
+"""
 
 
 import sys
 import os
 
 
-def markdown2html():
-    """
-    Script that converts Markdown to HTML
-
-    Args:
-        First argument is the name of the Markdown file.
-        Second argument is the output file name.
-
-    Returns:
-        Nothing.
-    """
+if __name__ == "__main__":
     if (len(sys.argv) < 3):
-        sys.stderr.write('Usage: ./markdown2html.py README.md README.html\n')
+        print("Usage: ./markdown2html.py README.md README.html",
+              file=sys.stderr)
         exit(1)
 
     md_file = sys.argv[1]
     html_file = sys.argv[2]
 
     if not(os.path.isfile("./{}".format(md_file))):
-        sys.stderr.write("Missing " + sys.argv[1] + '\n')
+        print("Missing {}".format(md_file), file=sys.stderr)
         exit(1)
 
     exit(0)
-
-if __name__ == "__main__":
-    markdown2html()
